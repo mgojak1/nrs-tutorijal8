8ae1f8bf4c0bbef654f5b07160c6e94cadec7670
Faris135 <48414463+Faris135@users.noreply.github.com>
2020-05-14 18:09:42 +0200

    napravljena baza

---
+++ db/baza.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('gradovi', 'root', '123', {host: '127.0.0.1', dialect: 'mysql', logging: false});
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Op = Sequelize.Op;
+
+// Import modela
+db.gradovi = sequelize.import(__dirname + '/gradovi.js');
+
+module.exports = db;
